35d0d0fdfbff2fc96f608198796440f49e90aad8
Your Name <you@example.com>
2022-05-30 15:22:01 +0400

    expenseFilter added

--- src/components/Expensees/Expenses.js
+++ src/components/Expensees/Expenses.js
@@ -1,10 +1,18 @@
 import './Expenses.css'
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import {useState} from "react";
 
 const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2020');
+    const filterChangeHndler = (selectedYear) => {
+        // console.log(selectedYear, 'expenses.js');
+        setFilteredYear(selectedYear);
+    }
     return(
         <Card className="expenses">
+            <ExpensesFilter selected ={filteredYear} onChangeFilter = {filterChangeHndler}/>
             <ExpenseItem title={props.item[0].title} amount={props.item[0].amount} date={props.item[0].date}/>
             <ExpenseItem title={props.item[1].title} amount={props.item[1].amount} date={props.item[1].date}/>
             <ExpenseItem title={props.item[2].title} amount={props.item[2].amount} date={props.item[2].date}/>
